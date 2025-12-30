eadf15b79c1713070b9b72f759182543164e26d0
Daniel Uohnson <peadser98@gmail.com>
2025-12-30 13:43:11 -0600

    practiqu

---
+++ pizza_order/logic.py
@@ -0,0 +1,20 @@
+pedido = []
+pizza = []
+opcion = str
+algo_mas = str
+
+hawiana = 8.5
+peperonni = 7.00
+jamonyq = 5.00
+
+salsa_de_tomate = 0.53
+queso_mozzarella = 1.21
+queso_parmesano = 0.96
+queso_cheddar = 0.43
+oregano = 0.21
+aceite_de_oliva = 0.11
+
+carrito = 0.00
+carrito = round(carrito, 2)
+cancelar = 0.00
+cancelar = round(cancelar, 2)
