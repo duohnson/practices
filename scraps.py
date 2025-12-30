4831257552f15de1477140e16aa053083afbcb85
Daniel Uohnson <peadser98@gmail.com>
2025-12-30 14:30:09 -0600

    sad

--- scraps.py
+++ scraps.py
@@ -1,5 +1,11 @@
-import random
+prueba = int(input("escriban\n"))
 
-numero_orden = random.randint(1, 100)
-
-print(numero_orden)
+match prueba:
+    case 1:
+        print("1")
+    case 2:
+        print("2")
+    case 3:
+        print("3")
+    case _:
+        print("valor erroneo, solo de 1 a 3")
