18e0a115067187cd7708226d5088e35de26d33c0
Daniel Uohnson <peadser98@gmail.com>
2025-12-31 14:55:55 -0600

    nocom

---
+++ json/practica.py
@@ -0,0 +1,22 @@
+import json
+########################################################
+########## PEQUEÑO EJEMPLO SOBRE LECTURA DICT ##########
+#json_str = '{"nombre":"daniel","apellidos":"uohnson"}'
+#convierte_a_dict=json.loads(json_str)
+#print(convierte_a_dict)
+#print(convierte_a_dict['nombre'])
+########################################################
+
+########################################################
+### COMO CARGAR ARCHIVOS DE UN DICT DE PYT A UN JSON ###
+#base = {
+#    "nombre":"daniel",
+#    "apellidos":"uohnson"
+#}
+
+#base_json = json.dumps(base)
+#print(base)
+########################################################
+
+########################################################
+#######################  ###############################
