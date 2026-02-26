29685aa4c5d68cfa19d33eb38b0e901a80b03a4b
duohnson <peadser98@gmail.com>
2026-02-25 20:21:21 -0600

    hello world

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOG: duohnson</title>
+</head>
+<body>
+    <h1>Bienvenido al BLOG de duohnson</h1>
+    <p>Este es un espacio donde comparto mis avances, aprendiendo a programar a la vieja escuela.</p>
+    <p>Aquí solo encontraras errores, código viejo y mal creado, spaghetti code y experimentos fallidos.</p>
+    <p>Es mi espacio donde yo puedo practicar a gusto y experimentar, sin miedo a equivocarme.</p>
+    <p>Solo uso PHP, CSS, HTML y JS puro, sin frameworks, sin copy & paste, todo hecho desde cero.</p>
+    <p>A LA OLDSCHOOL.</p>
+</body>
+</html>
